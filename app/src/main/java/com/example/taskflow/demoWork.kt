ff5573fdeb223605defa47604bac412361f7d6fd
kiran <betaastra112@gmail.com>
2024-07-04 21:45:46 +0545

    checking

---
+++ app/src/main/java/com/example/taskflow/demoWork.kt
@@ -0,0 +1,15 @@
+package com.example.taskflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class demoWork : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_demo_work)
+    }
+}
